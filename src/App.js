233c4c1eaf2c5174c2547731da2c84a8d3c59e12
Zidane Innis <zidaneinnis@gmail.com>
2022-07-28 13:01:34 -0500

    Finilized Header Options & Section build

--- src/App.js
+++ src/App.js
@@ -3,14 +3,18 @@ import React from "react";
 import { Counter } from "./features/counter/Counter";
 import "./App.css";
 import Header from "./Header";
+import Sidebar from "./Sidebar";
 
 function App() {
   return (
     <div className="app">
       <header className="App-header">
-        {/*Header*/}
         <Header />
-        {/*Sidebar*/}
+        {/*App Body*/}
+        <div className="app__body">
+          <Sidebar />
+        </div>
+
         {/*Feed*/}
         {/*Widgets*/}
       </header>
--- src/Header.js
+++ src/Header.js
@@ -1,6 +1,11 @@
 import React from "react";
 import "./Header.css";
 import SearchRoundedIcon from "@mui/icons-material/SearchRounded";
+import HomeRoundedIcon from "@mui/icons-material/HomeRounded";
+import SupervisorAccountRoundedIcon from "@mui/icons-material/SupervisorAccountRounded";
+import HeaderOption from "./HeaderOption";
+import NotificationsIcon from "@mui/icons-material/Notifications";
+import BusinessCenterIcon from "@mui/icons-material/BusinessCenter";
 function Header() {
   return (
     <div className="header">
@@ -11,7 +16,16 @@ function Header() {
         <SearchRoundedIcon />
         <input type="text" />
       </div>
-      <div className="header__right"></div>
+      <div className="header__right">
+        <HeaderOption Icon={HomeRoundedIcon} title="Home" />
+        <HeaderOption Icon={SupervisorAccountRoundedIcon} title="My Network" />
+        <HeaderOption Icon={BusinessCenterIcon} title="Jobs" />
+        <HeaderOption Icon={NotificationsIcon} title="Notifications" />
+        <HeaderOption
+          avatar="https://i.pinimg.com/564x/65/25/a0/6525a08f1df98a2e3a545fe2ace4be47.jpg"
+          title="me"
+        />
+      </div>
     </div>
   );
 }
