bff933e58ffeae51b8cd511866f80afc53c441e3
Zidane Innis <zidaneinnis@gmail.com>
2022-07-29 16:42:46 -0500

    Finished Feed Section
    Now building Input Section

--- src/Feed.js
+++ src/Feed.js
@@ -1,5 +1,11 @@
 import React from "react";
 import CreateIcon from "@mui/icons-material/Create";
+import "./Feed.css";
+import InputOption from "./InputOption";
+import ImageIcon from "@mui/icons-material/Image";
+import SubscriptionsIcon from "@mui/icons-material/Subscriptions";
+import EventNoteIcon from "@mui/icons-material/EventNote";
+import CalendarViewDayIcon from "@mui/icons-material/CalendarViewDay";
 function Feed() {
   return (
     <div className="feed">
@@ -10,6 +16,16 @@ function Feed() {
             <input type="text" />
             <button type="submit">Send</button>
           </form>
+        </div>
+        <div className="feed__inputOptions">
+          <InputOption Icon={ImageIcon} title="Photo" color="#70B5F9" />
+          <InputOption Icon={SubscriptionsIcon} title="Video" color="#E7A33E" />
+          <InputOption Icon={EventNoteIcon} title="Event" color="#C0CBCD" />
+          <InputOption
+            Icon={CalendarViewDayIcon}
+            title="Write Article"
+            color="#7FC15E"
+          />
         </div>
       </div>
     </div>
