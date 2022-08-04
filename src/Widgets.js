5f14566dafeef089cdd79743022841ca110323d1
Zidane Innis <zidaneinnis@gmail.com>
2022-08-03 20:14:46 -0500

    Finished Building the Widgets Section

--- src/Widgets.js
+++ src/Widgets.js
@@ -6,8 +6,9 @@ import FiberManualRecordIcon from "@mui/icons-material/FiberManualRecord";
 function Widgets() {
   const newsArticle = (heading, subtitle) => (
     <div className="widgets__article">
-      <div className="widgets__articleLeft"></div>
-      <FiberManualRecordIcon />
+      <div className="widgets__articleLeft">
+        <FiberManualRecordIcon />
+      </div>
       <div className="widgets__articleRight">
         <h4>{heading}</h4>
         <p>{subtitle}</p>
@@ -21,6 +22,11 @@ function Widgets() {
         <h2>LinkedIn News</h2>
         <InfoIcon />
       </div>
+      {newsArticle("Batman is back!", "Top news - 120,000 readers")}
+      {newsArticle("Bitcoin Rises", "Crypto - 14,000 readers")}
+      {newsArticle("Recession Looming?", "Finance - 10,000 readers")}
+      {newsArticle("New Webb Images ", "NASA - 21,000 readers")}
+      {newsArticle("React too powerful?", "Code - 12,000 readers")}
     </div>
   );
 }
